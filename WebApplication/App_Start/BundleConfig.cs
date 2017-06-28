9946b9fcfdaa81302f84ed91af207585c822e8e6
Bastien Debret <bastien@waykonect.com>
2017-06-28 12:02:41 +0200

    Further implementation of chords selection box

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -8,6 +8,13 @@ namespace WebApplication
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            InitiateScripts(bundles);
+            InitiateStyles(bundles);
+        }
+
+
+        private static void InitiateScripts(BundleCollection bundles)
+        {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -22,10 +29,12 @@ namespace WebApplication
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+        }
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+        private static void InitiateStyles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.css", "~/Content/site.css"));
         }
     }
 }
